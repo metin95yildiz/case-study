ba15e93ed6f1c6d0930039992d4566febb219416
metin95yildiz <metin_y1995@hotmail.com>
2021-08-02 01:16:51 +0300

    Edited next configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["source.unsplash.com"]
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   }
